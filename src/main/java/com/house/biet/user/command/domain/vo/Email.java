1e5dd1275fb0992e877b470b35290645cb5fce94
이학준 <dlgkrwns213@naver.com>
2026-01-29 00:11:01 +0900

    feat: Email VO 선언

---
+++ src/main/java/com/house/biet/user/command/domain/vo/Email.java
@@ -0,0 +1,27 @@
+package com.house.biet.user.command.domain.vo;
+
+import com.house.biet.global.response.CustomException;
+import com.house.biet.global.response.ErrorCode;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
+public class Email {
+
+    @Column(nullable = false, unique = true)
+    private String value;
+
+    public Email(String value) {
+        if (!value.contains("@") || !value.contains("."))
+            throw new CustomException(ErrorCode.INVALID_EMAIL_FORMAT);
+
+        this.value = value;
+    }
+}
