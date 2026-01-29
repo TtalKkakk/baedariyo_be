375af348d687bfe4f29e777a1fafe2eadfbe08ed
이학준 <dlgkrwns213@naver.com>
2026-01-29 23:09:33 +0900

    feat: User domain 선언

---
+++ src/main/java/com/house/biet/user/command/domain/entity/User.java
@@ -0,0 +1,37 @@
+package com.house.biet.user.command.domain.entity;
+
+import com.house.biet.user.command.domain.vo.Email;
+import com.house.biet.user.command.domain.vo.Password;
+import com.house.biet.user.command.domain.vo.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    private Email email;
+
+    @Embedded
+    private Password password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    public static User create(String email, String rawPassword, PasswordEncoder encoder) {
+        return new User(
+                null,
+                new Email(email),
+                Password.encrypt(rawPassword, encoder),
+                UserRole.USER
+        );
+    }
+}
