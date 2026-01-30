7b756f7823f8cab49de5357d7a5dfae820f4eaad
이학준 <dlgkrwns213@naver.com>
2026-01-31 03:36:06 +0900

    fix: SuccessCode 수정

--- src/main/java/com/house/biet/global/response/SuccessCode.java
+++ src/main/java/com/house/biet/global/response/SuccessCode.java
@@ -7,10 +7,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum SuccessCode {
 
+    // 공통
     OK(20000, "요청이 성공적으로 처리되었습니다."),
-    CREATED(20001, "정상적으로 생성되었습니다."),
-    UPDATED(20002, "젖상적으로 수정되었습니다."),
-    DELETED(20003, "정상적으로 삭제되었습니다.");
+
+    // Auth
+    SIGNUP_SUCCESS(21000, "회원가입이 완료되었습니다."),
+    LOGIN_SUCCESS(21001, "로그인에 성공했습니다."),
+    LOGOUT_SUCCESS(21002, "로그아웃 되었습니다."),
+
+    // User
+    USER_UPDATE_SUCCESS(22000, "회원 정보가 수정되었습니다.");
 
     private final int code;
     private final String message;
