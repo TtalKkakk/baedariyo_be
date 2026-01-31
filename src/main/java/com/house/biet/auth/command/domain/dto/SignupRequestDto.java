1ad312bd595687ac566fed39b5912ce4a90f5420
이학준 <dlgkrwns213@naver.com>
2026-02-01 01:42:53 +0900

    feat: SignupRequestDto 선언

---
+++ src/main/java/com/house/biet/auth/command/domain/dto/SignupRequestDto.java
@@ -0,0 +1,23 @@
+package com.house.biet.auth.command.domain.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record SignupRequestDto(
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        String password,
+
+        @NotBlank
+        String name,
+
+        @NotBlank
+        String nickname,
+
+        @NotBlank
+        String phoneNumber
+) {
+}
