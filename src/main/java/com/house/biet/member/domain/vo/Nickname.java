9299b0cb5b2447309c1d34ce5da19844b20544e5
이학준 <dlgkrwns213@naver.com>
2026-02-01 16:39:51 +0900

    feat: Nickname vo 선언

---
+++ src/main/java/com/house/biet/member/domain/vo/Nickname.java
@@ -0,0 +1,30 @@
+package com.house.biet.member.domain.vo;
+
+import com.house.biet.global.response.CustomException;
+import com.house.biet.global.response.ErrorCode;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
+public class Nickname {
+
+    private String value;
+
+    /**
+     * 닉네임은 5~15 자 사이이며 중복되지 않아야 함
+     *
+     * @param value: nickname
+     */
+    public Nickname(String value) {
+        if (value == null || value.isBlank())
+            throw new CustomException(ErrorCode.INVALID_NICK_NAME_FORMAT);
+        if (value.length() < 5 || value.length() > 15)
+            throw new CustomException(ErrorCode.INVALID_NICK_NAME_FORMAT);
+    }
+}
