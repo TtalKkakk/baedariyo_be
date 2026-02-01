77d2af987393912ab5cb62d69775b1e27599f2a1
이학준 <dlgkrwns213@naver.com>
2026-02-01 19:28:25 +0900

    fix: User 에 이메일 비밀번호 삭제 및 전화번호 추가

--- src/main/java/com/house/biet/user/command/domain/entity/User.java
+++ src/main/java/com/house/biet/user/command/domain/entity/User.java
@@ -1,13 +1,8 @@
 package com.house.biet.user.command.domain.entity;
 
-import com.house.biet.member.command.domain.vo.Email;
-import com.house.biet.member.command.domain.vo.Nickname;
-import com.house.biet.member.command.domain.vo.Password;
-import com.house.biet.global.vo.UserRole;
-import com.house.biet.member.command.domain.vo.RealName;
+import com.house.biet.member.command.domain.vo.*;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Entity
 @Table(name = "users")
@@ -18,13 +13,6 @@ public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Embedded
-    @AttributeOverride(
-            name = "value",
-            column = @Column(name = "email", nullable = false, unique = true)
-    )
-    private Email email;
 
     @Embedded
     @AttributeOverride(
@@ -43,22 +31,16 @@ public class User {
     @Embedded
     @AttributeOverride(
             name = "value",
-            column = @Column(name = "password", nullable = false)
+            column = @Column(name = "phoneNumber", nullable = false, unique = true)
     )
-    private Password password;
+    private PhoneNumber phoneNumber;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private UserRole role;
-
-    public static User create(String email, String realName, String nickname, String rawPassword, PasswordEncoder encoder) {
+    public static User create(String realName, String nickname, String phoneNumber) {
         return new User(
                 null,
-                new Email(email),
                 new RealName(realName),
                 new Nickname(nickname),
-                Password.encrypt(rawPassword, encoder),
-                UserRole.USER
+                new PhoneNumber(phoneNumber)
         );
     }
 }
