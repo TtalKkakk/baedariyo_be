f69f9487e1feb09aa0d13204c7eb5397d772ed34
이학준 <dlgkrwns213@naver.com>
2026-02-02 03:31:19 +0900

    test: User 수정에 따른 test 코드 추가

--- src/test/java/com/house/biet/user/command/domain/entity/UserTest.java
+++ src/test/java/com/house/biet/user/command/domain/entity/UserTest.java
@@ -1,5 +1,7 @@
 package com.house.biet.user.command.domain.entity;
 
+import com.house.biet.member.command.domain.vo.Nickname;
+import com.house.biet.member.command.domain.vo.PhoneNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,5 +24,43 @@ class UserTest {
         assertThat(user.getRealName().getValue()).isEqualTo(givenRealNameValue);
         assertThat(user.getNickname().getValue()).isEqualTo(givenNickNameValue);
         assertThat(user.getPhoneNumber().getValue()).isEqualTo(givenPhoneNumberValue);
+    }
+
+    @Test
+    @DisplayName("성공 - 유저 닉네임 변경 성공")
+    void changeNickname_Success() {
+        // given
+        String givenRealNameValue = "<REAL_NAME>";
+        String givenNickNameValue = "<NICK_NAME>";
+        String givenPhoneNumberValue = "010-1111-1111";
+
+        User user = User.create(givenRealNameValue, givenNickNameValue, givenPhoneNumberValue);
+
+        Nickname newNickname = new Nickname("<NEW_NICKNAME>");
+
+        // when
+        user.changeNickname(newNickname);
+
+        // then
+        assertThat(user.getNickname()).isEqualTo(newNickname);
+    }
+    
+    @Test
+    @DisplayName("성공 - 유저 휴대전화번호 변경 성공")
+    void changePhoneNumber_Success() {
+        // given
+        String givenRealNameValue = "<REAL_NAME>";
+        String givenNickNameValue = "<NICK_NAME>";
+        String givenPhoneNumberValue = "010-1111-1111";
+
+        User user = User.create(givenRealNameValue, givenNickNameValue, givenPhoneNumberValue);
+
+        PhoneNumber newPhoneNumber = new PhoneNumber("010-1111-1112");
+
+        // when
+        user.changePhoneNumber(newPhoneNumber);
+
+        // then
+        assertThat(user.getPhoneNumber()).isEqualTo(newPhoneNumber);
     }
 }
