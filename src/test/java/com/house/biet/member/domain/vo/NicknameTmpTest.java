7137eacd752066029bbb06e3d98d47b1482a9f47
이학준 <dlgkrwns213@naver.com>
2026-02-01 17:06:22 +0900

    test: Nickname test 구현

---
+++ src/test/java/com/house/biet/member/domain/vo/NicknameTmpTest.java
@@ -0,0 +1,50 @@
+package com.house.biet.member.domain.vo;
+
+import com.house.biet.global.response.CustomException;
+import com.house.biet.global.response.ErrorCode;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NicknameTmpTest {
+    @Test
+    @DisplayName("성공 - NickName vo 생성")
+    void CreateNicknameVO_Success() {
+        // given
+        String value5 = "abcde";
+        String value15 = "abcdefghijklmno";
+
+        // when
+        Nickname nickname5 = new Nickname(value5);
+        Nickname nickname15 = new Nickname(value15);
+
+        // then
+        assertThat(nickname5).isNotNull();
+        assertThat(nickname15).isInstanceOf(Nickname.class);
+    }
+
+    @Test
+    @DisplayName("실패 - Nickname VO에 not value 및 길이 에러")
+    void CreateNicknameVO_Error_NotValue() {
+        // given
+        String blankValue = "";
+        String value4 = "abcd";
+        String value16 = "abcdefghijklmnop";
+
+        // when & then
+        assertThatThrownBy(() -> new Nickname(blankValue))
+                .isInstanceOf(CustomException.class)
+                .hasMessage(ErrorCode.INVALID_NICK_NAME_FORMAT.getMessage());
+
+        assertThatThrownBy(() -> new Nickname(value4))
+                .isInstanceOf(CustomException.class)
+                .hasMessage(ErrorCode.INVALID_NICK_NAME_FORMAT.getMessage());
+
+        assertThatThrownBy(() -> new Nickname(value16))
+                .isInstanceOf(CustomException.class)
+                .hasMessage(ErrorCode.INVALID_NICK_NAME_FORMAT.getMessage());
+    }
+}
