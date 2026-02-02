e25b2f0740407045afa5d0345671811e913c875f
이학준 <dlgkrwns213@naver.com>
2026-02-03 03:00:50 +0900

    feat: RiderStatus 선언

---
+++ src/main/java/com/house/biet/rider/command/domain/vo/RiderStatus.java
@@ -0,0 +1,9 @@
+package com.house.biet.rider.command.domain.vo;
+
+public enum RiderStatus {
+    OFFLINE,    // 로그인 안 함
+    ONLINE,     // 대기 중
+    WORKING,    // 배달 중
+    SUSPENDED,  // 제재
+    WITHDRAWN   // 탈퇴
+}
