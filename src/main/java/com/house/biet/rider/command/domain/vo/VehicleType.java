4d7d90d33bb2d09748325b914786484965394d09
이학준 <dlgkrwns213@naver.com>
2026-02-03 03:01:01 +0900

    feat: VehicleType 선언

---
+++ src/main/java/com/house/biet/rider/command/domain/vo/VehicleType.java
@@ -0,0 +1,12 @@
+package com.house.biet.rider.command.domain.vo;
+
+public enum VehicleType {
+    CAR,            // 자동차
+    MOTORCYCLE,     // 오토바이
+    BICYCLE,        // 자전거
+    E_BICYCLE,      // 전기자전거
+    E_SCOOTER,      // 전동 킥보드
+    WALKING,        // 도보
+    ETC             // 기타
+}
+
