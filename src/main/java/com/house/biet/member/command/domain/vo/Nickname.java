33d71d3a610455f35f43f8150ac6f048889a4601
이학준 <dlgkrwns213@naver.com>
2026-02-02 17:13:29 +0900

    feat: Nickname 최대길이 제한을 30자로 수정

--- src/main/java/com/house/biet/member/command/domain/vo/Nickname.java
+++ src/main/java/com/house/biet/member/command/domain/vo/Nickname.java
@@ -17,14 +17,14 @@ public class Nickname {
     private String value;
 
     /**
-     * 닉네임은 5~15 자 사이이며 중복되지 않아야 함
+     * 닉네임은 5~30 자 사이이며 중복되지 않아야 함
      *
      * @param value: nickname
      */
     public Nickname(String value) {
         if (value == null || value.isBlank())
             throw new CustomException(ErrorCode.INVALID_NICK_NAME_FORMAT);
-        if (value.length() < 5 || value.length() > 15)
+        if (value.length() < 5 || value.length() > 30)
             throw new CustomException(ErrorCode.INVALID_NICK_NAME_FORMAT);
 
         this.value = value;
