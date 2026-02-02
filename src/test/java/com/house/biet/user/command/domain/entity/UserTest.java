f8ba0a0a7c83e965a1ed0f5a4e011e47fc6c8191
이학준 <dlgkrwns213@naver.com>
2026-02-03 03:03:00 +0900

    test: UserTest 코드 수정

--- src/test/java/com/house/biet/user/command/domain/entity/UserTest.java
+++ src/test/java/com/house/biet/user/command/domain/entity/UserTest.java
@@ -9,14 +9,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class UserTest {
 
+    String givenRealNameValue = "<REAL_NAME>";
+    String givenNickNameValue = "<NICK_NAME>";
+    String givenPhoneNumberValue = "010-1111-1111";
+
     @Test
     @DisplayName("성공 - 유저 생성 성공")
     void CreateUser_Success() {
-        // given
-        String givenRealNameValue = "<REAL_NAME>";
-        String givenNickNameValue = "<NICK_NAME>";
-        String givenPhoneNumberValue = "010-1111-1111";
-
         // when
         User user = User.create(givenRealNameValue, givenNickNameValue, givenPhoneNumberValue);
 
@@ -30,10 +29,6 @@ class UserTest {
     @DisplayName("성공 - 유저 닉네임 변경 성공")
     void changeNickname_Success() {
         // given
-        String givenRealNameValue = "<REAL_NAME>";
-        String givenNickNameValue = "<NICK_NAME>";
-        String givenPhoneNumberValue = "010-1111-1111";
-
         User user = User.create(givenRealNameValue, givenNickNameValue, givenPhoneNumberValue);
 
         Nickname newNickname = new Nickname("<NEW_NICKNAME>");
@@ -49,10 +44,6 @@ class UserTest {
     @DisplayName("성공 - 유저 휴대전화번호 변경 성공")
     void changePhoneNumber_Success() {
         // given
-        String givenRealNameValue = "<REAL_NAME>";
-        String givenNickNameValue = "<NICK_NAME>";
-        String givenPhoneNumberValue = "010-1111-1111";
-
         User user = User.create(givenRealNameValue, givenNickNameValue, givenPhoneNumberValue);
 
         PhoneNumber newPhoneNumber = new PhoneNumber("010-1111-1112");
