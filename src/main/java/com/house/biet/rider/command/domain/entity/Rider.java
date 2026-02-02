b0bd1af076ff67d9f61f06328478af2ff71d0fa1
이학준 <dlgkrwns213@naver.com>
2026-02-03 03:01:20 +0900

    feat: Rider domain 선언

---
+++ src/main/java/com/house/biet/rider/command/domain/entity/Rider.java
@@ -0,0 +1,71 @@
+package com.house.biet.rider.command.domain.entity;
+
+import com.house.biet.global.jpa.BaseTimeEntity;
+import com.house.biet.member.command.domain.vo.Nickname;
+import com.house.biet.member.command.domain.vo.PhoneNumber;
+import com.house.biet.member.command.domain.vo.RealName;
+import com.house.biet.rider.command.domain.vo.VehicleType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "riders")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Rider extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    @AttributeOverride(
+            name = "value",
+            column = @Column(name = "real_name", nullable = false)
+    )
+    private RealName realName;
+
+    @Embedded
+    @AttributeOverride(
+            name = "value",
+            column = @Column(name = "nickname", nullable = false, unique = true)
+    )
+    private Nickname nickname;
+
+
+    @Embedded
+    @AttributeOverride(
+            name = "value",
+            column = @Column(name = "phoneNumber", nullable = false, unique = true)
+    )
+    private PhoneNumber phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private VehicleType vehicleType;
+
+    public static Rider create(String realName, String nickname, String phoneNumber, VehicleType vehicleType) {
+        return new Rider(
+                null,
+                new RealName(realName),
+                new Nickname(nickname),
+                new PhoneNumber(phoneNumber),
+                vehicleType
+        );
+    }
+
+    public void changeNickname(Nickname nickname) {
+        this.nickname = nickname;
+    }
+
+    public void changePhoneNumber(PhoneNumber phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void changeVehicleType(VehicleType vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+}
