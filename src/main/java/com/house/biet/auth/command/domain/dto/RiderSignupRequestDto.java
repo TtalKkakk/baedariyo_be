a8ec6f50257ef75bcb125d78456cc56cffcd3d86
이학준 <dlgkrwns213@naver.com>
2026-02-04 03:27:59 +0900

    feat: RiderSignupRequestDto 선언

---
+++ src/main/java/com/house/biet/auth/command/domain/dto/RiderSignupRequestDto.java
@@ -0,0 +1,27 @@
+package com.house.biet.auth.command.domain.dto;
+
+import com.house.biet.rider.command.domain.vo.VehicleType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record RiderSignupRequestDto(
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        String password,
+
+        @NotBlank
+        String name,
+
+        @NotBlank
+        String nickname,
+
+        @NotBlank
+        String phoneNumber,
+
+        @NotBlank
+        VehicleType vehicleType
+) {
+}
