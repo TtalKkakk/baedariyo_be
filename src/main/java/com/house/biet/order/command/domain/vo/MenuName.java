500879224a9a202987283dc3efa86da9250945f2
이학준 <dlgkrwns213@naver.com>
2026-02-06 01:29:58 +0900

    feat: menu name VO 선언

---
+++ src/main/java/com/house/biet/order/command/domain/vo/MenuName.java
@@ -0,0 +1,39 @@
+package com.house.biet.order.command.domain.vo;
+
+import com.house.biet.global.response.CustomException;
+import com.house.biet.global.response.ErrorCode;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 메뉴 이름 값 객체
+ *
+ * <p>
+ * - 주문 시점의 메뉴 이름 스냅샷
+ * - 유효성 검증을 생성 시점에 수행
+ * </p>
+ */
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
+public class MenuName {
+
+    private String value;
+
+    public MenuName(String value) {
+        if (value == null || value.isBlank())
+            throw new CustomException(ErrorCode.INVALID_MENU_NAME_FORMAT);
+        if (value.length() > 50)
+            throw new CustomException(ErrorCode.INVALID_MENU_NAME_FORMAT);
+
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
+}
