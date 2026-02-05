daf2711ecc17e3f7fed5264ccc3356130b53a0ad
이학준 <dlgkrwns213@naver.com>
2026-02-06 01:30:35 +0900

    feat: order menu vo 선언

---
+++ src/main/java/com/house/biet/order/command/domain/vo/OrderMenu.java
@@ -0,0 +1,82 @@
+package com.house.biet.order.command.domain.vo;
+
+import com.house.biet.global.response.CustomException;
+import com.house.biet.global.response.ErrorCode;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 주문 메뉴 값 객체
+ *
+ * <p>
+ * - 한 주문에 포함된 단일 메뉴를 표현
+ * - 메뉴 가격, 옵션, 수량은 주문 시점 기준으로 고정
+ * - Order Aggregate에 완전히 종속됨
+ * </p>
+ */
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = {
+        "storeId",
+        "menuId",
+        "menuName",
+        "quantity",
+        "menuPrice",
+})
+public class OrderMenu {
+
+    /**
+     * 가게 ID (한 주문 = 한 가게 정책 검증용)
+     */
+    private Long storeId;
+
+    /**
+     * 메뉴 ID
+     */
+    private Long menuId;
+
+    /**
+     * 메뉴 이름
+     */
+    private MenuName menuName;
+
+    /**
+     * 주문 수량
+     */
+    private int quantity;
+
+    /**
+     * 메뉴 기본 가격
+     */
+    @Embedded
+    private Money menuPrice;
+
+    public OrderMenu(Long storeId, Long menuId, MenuName menuName, int quantity, Money menuPrice) {
+        if (quantity <= 0)
+            throw new CustomException(ErrorCode.INVALID_MENU_QUANTITY);
+
+        this.storeId = storeId;
+        this.menuId = menuId;
+        this.menuName = menuName;
+        this.quantity = quantity;
+        this.menuPrice = menuPrice;
+    }
+
+    /**
+     * 주문 메뉴의 총 가격 계산
+     *
+     * <p>
+     * 기본 가격 × 수량
+     * </p>
+     *
+     * @return 계산된 총 금액
+     */
+    public Money totalPrice() {
+        return menuPrice.multiply(quantity);
+    }
+}
