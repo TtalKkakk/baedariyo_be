d16ccf1efb21fe277b936e22f221d74682a1361b
이학준 <dlgkrwns213@naver.com>
2026-02-06 00:20:04 +0900

    feat: 음수 Money Amount 에러코드 설정

--- src/main/java/com/house/biet/global/response/ErrorCode.java
+++ src/main/java/com/house/biet/global/response/ErrorCode.java
@@ -17,6 +17,7 @@ public enum ErrorCode {
     INVALID_NICK_NAME_FORMAT(40106, HttpStatus.BAD_REQUEST, "잘못된 사용자 닉네임 형식입니다."),
     INVALID_PHONE_NUMBER_FORMAT(40107, HttpStatus.BAD_REQUEST, "잘못된 휴대전화번호 형식입니다."),
     ALREADY_WITHDRAWN_ACCOUNT(40108, HttpStatus.BAD_REQUEST, "이미 삭제된 계정입니다."),
+    INVALID_MONEY_AMOUNT(40109, HttpStatus.BAD_REQUEST, "금액은 음수일 수 없습니다."),
     BAD_REQUEST(40100, HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
 
     // 401 Unauthorized
