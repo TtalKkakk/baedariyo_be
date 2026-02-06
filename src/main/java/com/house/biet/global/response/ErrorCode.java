f2d62cafe61df96cbcf71595a9eee3e2f3e7ae12
이학준 <dlgkrwns213@naver.com>
2026-02-07 04:19:55 +0900

    feat: Order NotFound 에러코드 추가

--- src/main/java/com/house/biet/global/response/ErrorCode.java
+++ src/main/java/com/house/biet/global/response/ErrorCode.java
@@ -41,6 +41,7 @@ public enum ErrorCode {
     USER_NOT_FOUND(40402, HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
     RIDER_NOT_FOUND(40403, HttpStatus.NOT_FOUND, "라이더를 찾을 수 없습니다."),
     ORDER_MENU_NOT_FOUND(40404, HttpStatus.NOT_FOUND, "주문 메뉴를 찾을 수 없습니다."),
+    ORDER_NOT_FOUND(40405, HttpStatus.NOT_FOUND, "주문을 찾을 수 없습니다."),
     NOT_FOUND(40400, HttpStatus.NOT_FOUND, "찾을 수 없습니다."),
 
     // 409 Conflict
