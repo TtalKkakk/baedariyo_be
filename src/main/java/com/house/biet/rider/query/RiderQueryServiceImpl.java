d70ced2d62155382dbb25d5ade676aea6e9b02cd
이학준 <dlgkrwns213@naver.com>
2026-02-09 00:22:28 +0900

    feat: RiderQueryService 구현

---
+++ src/main/java/com/house/biet/rider/query/RiderQueryServiceImpl.java
@@ -0,0 +1,21 @@
+package com.house.biet.rider.query;
+
+import com.house.biet.global.response.CustomException;
+import com.house.biet.global.response.ErrorCode;
+import com.house.biet.member.command.domain.vo.Nickname;
+import com.house.biet.rider.command.RiderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RiderQueryServiceImpl implements RiderQueryService {
+
+    private final RiderRepository riderRepository;
+
+    @Override
+    public Long getRiderIdByNickname(String nicknameValue) {
+        return riderRepository.findRiderIdByNickname(nicknameValue)
+                .orElseThrow(() -> new CustomException(ErrorCode.RIDER_NOT_FOUND));
+    }
+}
