55dc85381fd279204e23e6e81a312e6b27112f69
이학준 <dlgkrwns213@naver.com>
2026-02-09 01:25:22 +0900

    feat: OrderAssignService 구현

---
+++ src/main/java/com/house/biet/order/command/domain/application/OrderAssignService.java
@@ -0,0 +1,31 @@
+package com.house.biet.order.command.domain.application;
+
+import com.house.biet.global.response.CustomException;
+import com.house.biet.global.response.ErrorCode;
+import com.house.biet.order.command.OrderRepository;
+import com.house.biet.order.command.domain.aggregate.Order;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderAssignService {
+
+    private final OrderRepository orderRepository;
+
+    /**
+     * 주문에 라이더를 배정한다.
+     *
+     * @param orderId 주문 ID
+     * @param riderId 라이더 ID
+     * @throws CustomException 주문이 존재하지 않는 경우
+     */
+    @Transactional
+    public void assignRider(Long orderId, Long riderId) {
+        Order order = orderRepository.findByIdForUpdate(orderId)
+                .orElseThrow(() -> new CustomException(ErrorCode.ORDER_NOT_FOUND));
+
+        order.assignRider(riderId);
+    }
+}
