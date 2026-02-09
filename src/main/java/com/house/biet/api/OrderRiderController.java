97d5080863d5666bd63219ba2b1b0d8770e24522
이학준 <dlgkrwns213@naver.com>
2026-02-10 00:58:39 +0900

    feat: OrderRiderController 에서 라이더 부여 api 추가

---
+++ src/main/java/com/house/biet/api/OrderRiderController.java
@@ -0,0 +1,37 @@
+package com.house.biet.api;
+
+import com.house.biet.auth.infrastructure.security.AuthPrincipal;
+import com.house.biet.global.response.CustomApiResponse;
+import com.house.biet.global.response.SuccessCode;
+import com.house.biet.order.command.domain.application.OrderRiderFacade;
+import com.house.biet.order.command.domain.dto.OrderRiderAssignRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/orders/users")
+public class OrderRiderController {
+
+    private OrderRiderFacade orderRiderFacade;
+
+    @PostMapping("/create")
+    public ResponseEntity<CustomApiResponse<Void>> assignRider(
+            @AuthenticationPrincipal AuthPrincipal principal,
+            @RequestBody OrderRiderAssignRequestDto requestDto
+    ) {
+        orderRiderFacade.assignRider(
+                principal.accountId(),
+                requestDto.orderId()
+        );
+
+        return ResponseEntity.ok(
+                CustomApiResponse.success(SuccessCode.ORDER_CREATE_SUCCESS)
+        );
+    }
+}
