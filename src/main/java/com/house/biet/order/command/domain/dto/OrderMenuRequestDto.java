69d611a25199dd2d65e2bb83be41524f004ff0f2
이학준 <dlgkrwns213@naver.com>
2026-02-09 16:38:58 +0900

    feat: 메뉴 이름과 가격도 받도록 설정

--- src/main/java/com/house/biet/order/command/domain/dto/OrderMenuRequestDto.java
+++ src/main/java/com/house/biet/order/command/domain/dto/OrderMenuRequestDto.java
@@ -1,12 +1,19 @@
 package com.house.biet.order.command.domain.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record OrderMenuRequestDto(
 
         @NotNull
         Long menuId,
+
+        @NotBlank
+        String menuName,
+
+        @Min(0)
+        int menuPrice,
 
         @Min(1)
         int quantity
