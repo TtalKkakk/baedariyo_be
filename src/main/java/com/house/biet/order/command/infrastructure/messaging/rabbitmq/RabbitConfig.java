9c08ffcb1c7be3456c8dd01c46c29a16866be577
이학준 <dlgkrwns213@naver.com>
2026-02-14 21:08:57 +0900

    chore: RabbitConfig 구현

---
+++ src/main/java/com/house/biet/order/command/infrastructure/messaging/rabbitmq/RabbitConfig.java
@@ -0,0 +1,31 @@
+package com.house.biet.order.command.infrastructure.messaging.rabbitmq;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RabbitConfig {
+
+    @Bean
+    public TopicExchange orderExchange() {
+        return new TopicExchange("order.exchange");
+    }
+
+    @Bean
+    public Queue riderNotificationQueue() {
+        return QueueBuilder.durable("rider.notification.queue")
+                .withArgument("x-message-ttl", 30000)
+                .withArgument("x-dead-letter-exchange", "order.dlx")
+                .build();
+    }
+
+    @Bean
+    public Binding riderBinding() {
+        return BindingBuilder
+                .bind(riderNotificationQueue())
+                .to(orderExchange())
+                .with("order.created.seoul.*");
+    }
+}
