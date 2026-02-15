a9db1d7df64d50639e342ae7b80af350cf2b2c4b
이학준 <dlgkrwns213@naver.com>
2026-02-15 21:12:50 +0900

    feat: Rabbit 관련 설정 이름 수정

--- src/main/java/com/house/biet/order/command/infrastructure/messaging/rabbitmq/RabbitTopologyConfig.java
+++ src/main/java/com/house/biet/order/command/infrastructure/messaging/rabbitmq/RabbitTopologyConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-public class RabbitConfig {
+public class RabbitTopologyConfig {
 
     @Bean
     public TopicExchange orderExchange() {
