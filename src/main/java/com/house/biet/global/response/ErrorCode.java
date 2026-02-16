edc4a122e0bce1860e5d3e8fc4aa01443b3a544b
이학준 <dlgkrwns213@naver.com>
2026-02-16 21:47:02 +0900

    feat: MenuName 형식 에러 코드 추가

--- src/main/java/com/house/biet/global/response/ErrorCode.java
+++ src/main/java/com/house/biet/global/response/ErrorCode.java
@@ -36,6 +36,7 @@ public enum ErrorCode {
     INVALID_MENU_OPTION_NAME_FORMAT(40125, HttpStatus.BAD_REQUEST, "잘못된 메뉴 옵션 이름 형식입니다."),
     EXCEED_MAX_OPTION_SELECTABLE_COUNT(40126, HttpStatus.BAD_REQUEST, "메뉴 옵션 최대치를 초과하였습니다."),
     INVALID_STORE_NAME_FORMAT(40127, HttpStatus.BAD_REQUEST, "잘못된 가게 이름 형식입니다."),
+    INVALID_MENU_NAME_FORMAT(40128, HttpStatus.BAD_REQUEST, "잘못된 메뉴 이름 형식입니다."),
     BAD_REQUEST(40100, HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
 
     // 401 Unauthorized
