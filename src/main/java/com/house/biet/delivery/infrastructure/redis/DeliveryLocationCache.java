a3c5acfc7178a406fbad580c2c53dd496e0dda4d
이학준 <dlgkrwns213@naver.com>
2026-03-08 01:13:53 +0900

    feat: record 로 수정

--- src/main/java/com/house/biet/delivery/infrastructure/redis/DeliveryLocationCache.java
+++ src/main/java/com/house/biet/delivery/infrastructure/redis/DeliveryLocationCache.java
@@ -1,19 +1,11 @@
 package com.house.biet.delivery.infrastructure.redis;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class DeliveryLocationCache implements Serializable {
-
-    private Long riderId;
-    private double latitude;
-    private double longitude;
-    private LocalDateTime updatedAt;
-}
+public record DeliveryLocationCache(
+        Long riderId,
+        double latitude,
+        double longitude,
+        LocalDateTime updatedAt
+) implements Serializable {}
