18bf331fe275afc36e019ad83b83910c490c7ff6
이학준 <dlgkrwns213@naver.com>
2026-03-08 00:12:47 +0900

    fix: 필요없는 controller 삭제 및 api 수정

--- src/main/java/com/house/biet/api/OrderUserController.java
+++ src/main/java/com/house/biet/api/OrderUserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/orders/rider")
+@RequestMapping("/api/orders/users")
 public class OrderUserController {
 
     private final OrderUserFacade orderUserFacade;
