e5c92e7822ef7b30ec1dc23875b7d4536055ffb3
이학준 <dlgkrwns213@naver.com>
2026-03-11 21:16:28 +0900

    fix: api 수정

--- src/main/java/com/house/biet/api/DeliveryLocationController.java
+++ src/main/java/com/house/biet/api/DeliveryLocationController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/deliveries")
+@RequestMapping("/api/deliveries")
 public class DeliveryLocationController {
 
     private final DeliveryLocationService deliveryLocationService;
