f19bed483d878205c84a3e25edbf644256256128
이학준 <dlgkrwns213@naver.com>
2026-03-11 21:16:28 +0900

    fix: api 수정

--- src/main/java/com/house/biet/api/DeliveryRiderController.java
+++ src/main/java/com/house/biet/api/DeliveryRiderController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/deliveries")
+@RequestMapping("/api/deliveries")
 public class DeliveryRiderController {
 
     private final DeliveryOrderFacade deliveryOrderFacade;
@@ -49,4 +49,5 @@ public class DeliveryRiderController {
                 CustomApiResponse.success(SuccessCode.DELIVERY_COMPLETE_SUCCESS)
         );
     }
+
 }
