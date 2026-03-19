47ca0995be48a157fd0e6a2560aca10c165474a2
이학준 <dlgkrwns213@naver.com>
2026-03-19 23:41:45 +0900

    feat: RiderCommandFacade 선언

---
+++ src/main/java/com/house/biet/rider/command/application/RiderCommandFacade.java
@@ -0,0 +1,92 @@
+package com.house.biet.rider.command.application;
+
+import com.house.biet.common.domain.enums.RiderWorkingStatus;
+import com.house.biet.common.domain.enums.VehicleType;
+import com.house.biet.rider.query.RiderQueryService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * 라이더 관련 Command 요청을 처리하는 Facade 클래스입니다.
+ *
+ * <p>
+ * AccountId를 기반으로 RiderId를 조회한 뒤,
+ * 실제 비즈니스 로직은 {@link RiderService}에 위임합니다.
+ * </p>
+ *
+ * <p>
+ * 역할:
+ * <ul>
+ *     <li>Account → Rider 식별자 변환</li>
+ *     <li>Command 서비스로 위임</li>
+ * </ul>
+ * </p>
+ */
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class RiderCommandFacade {
+
+    private final RiderService riderService;
+    private final RiderQueryService riderQueryService;
+
+    /**
+     * 라이더 닉네임을 변경합니다.
+     *
+     * @param accountId 계정 ID
+     * @param newNicknameValue 변경할 닉네임
+     */
+    public void changeNicknameByRiderId(Long accountId, String newNicknameValue) {
+        Long riderId = riderQueryService.getRiderIdByAccountId(accountId);
+
+        riderService.changeNicknameByRiderId(riderId, newNicknameValue);
+    }
+
+    /**
+     * 라이더 전화번호를 변경합니다.
+     *
+     * @param accountId 계정 ID
+     * @param newPhoneNumberValue 변경할 전화번호
+     */
+    public void changePhoneNumberByRiderId(Long accountId, String newPhoneNumberValue) {
+        Long riderId = riderQueryService.getRiderIdByAccountId(accountId);
+
+        riderService.changePhoneNumberByRiderId(riderId, newPhoneNumberValue);
+    }
+
+    /**
+     * 라이더의 차량 유형을 변경합니다.
+     *
+     * @param accountId 계정 ID
+     * @param newVehicleType 변경할 차량 유형
+     */
+    public void changeVehicleType(Long accountId, VehicleType newVehicleType) {
+        Long riderId = riderQueryService.getRiderIdByAccountId(accountId);
+
+        riderService.changeVehicleType(riderId, newVehicleType);
+    }
+
+    /**
+     * 라이더의 근무 상태를 변경합니다.
+     *
+     * @param accountId 계정 ID
+     * @param newRiderWorkingStatus 변경할 근무 상태
+     */
+    public void changeRiderWorkingStatus(Long accountId, RiderWorkingStatus newRiderWorkingStatus) {
+        Long riderId = riderQueryService.getRiderIdByAccountId(accountId);
+
+        riderService.changeRiderWorkingStatus(riderId, newRiderWorkingStatus);
+    }
+
+    /**
+     * 현재 오프라인 상태라면 온라인으로 변경합니다.
+     *
+     * @param accountId 계정 ID
+     */
+    public void markOnlineIfOffline(Long accountId) {
+        Long riderId = riderQueryService.getRiderIdByAccountId(accountId);
+
+        riderService.markOnlineIfOffline(riderId);
+    }
+}
